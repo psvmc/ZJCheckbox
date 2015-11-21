48f15e41a47e4d440abc415c135b28df4283910a
psvmc <183518918@qq.com>
2015-11-21 16:37:45 +0800

    Checkbox组件

---
+++ ZJCheckbox/ZJCheckboxDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  ZJCheckboxDelegate.swift
+//
+//  Created by PSVMC on 11/18/15.
+//  Copyright (c) 2015 PSVMC. All rights reserved.
+//
+
+import Foundation
+
+protocol ZJCheckboxDelegate {
+    func didSelectCheckbox(state: Bool, identifier: Int, title: String);
+}
